26a728e1053f6d599ac39cfe862c0c7c5be02c66
William <William@LAPTOP-EI9NU3MM.hky.com>
2019-02-07 17:54:40 -0800

    First Commit

---
+++ src/githubhello/GithubHello.java
@@ -0,0 +1,17 @@
+package githubhello;
+
+public class GithubHello {
+
+    public static void main(String[] args) {
+        
+        Cowden();
+        
+    }
+    
+    public static void Cowden() {
+        
+        System.out.println("William Cowden!");
+        
+    }
+    
+}
